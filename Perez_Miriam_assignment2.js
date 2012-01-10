2c3a50d9b2828cfbcfb04b60185cc7e83b399341
Miriam Perez <mperez1227@fullsail.edu>
2012-01-09 23:12:52 -0500

    added some variables and procedure

--- Perez_Miriam_assignment2.js
+++ Perez_Miriam_assignment2.js
@@ -6,3 +6,22 @@ Deliverable 2
 
 //alert("JavaScript works!");
 
+var day = "Friday",
+	destination = "movies",
+	movieType1 = "comedy",
+	movieType2 = "action",
+	thisFriday = true;
+
+//Procedure
+
+console.log("We are going out this " + day + ", so we will only go to the " + destination + ".");
+
+if (thisFriday === true) {
+
+	console.log("I want to see a good " + movieType1 + ".");
+
+} else {
+
+	console.log("We will end up watching an " + movieType2 + " film.");
+};
+
